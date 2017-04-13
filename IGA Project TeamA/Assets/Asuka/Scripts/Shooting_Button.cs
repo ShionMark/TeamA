283387a0da1fdc35f88ca3987196410afe347229
KBC11606120\kbc11606120 <asuka-m-16d@stu.kbc.ac.jp>
2017-04-13 15:50:56 +0900

    弾を発射できます（完全自動）

---
+++ IGA Project TeamA/Assets/Asuka/Scripts/Shooting_Button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting_Button : MonoBehaviour {
+
+
+    public void OnClick()
+    {
+        Debug.Log("Button click!");
+    }
+}
