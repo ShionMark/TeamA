550a4f588451ad19915d7c2b97873cdd4a03b412
KBC11606120\kbc11606120 <asuka-m-16d@stu.kbc.ac.jp>
2017-04-14 15:58:51 +0900

    弾の方向 修正

--- IGA Project TeamA/Assets/MainFolder/Asuka/Scripts/Shooting.cs
+++ IGA Project TeamA/Assets/MainFolder/Asuka/Scripts/Shooting.cs
@@ -11,26 +11,31 @@ namespace Itsuki
         // 弾丸発射点
         public Transform muzzlemain;
         public Transform muzzlesub;
+
+        public Transform enemyposi;
         // 弾丸の速度
-        public float speed = 1000;
+        public float speed = 100;
 
         IEnumerator Start()
         {
             while (true)
             {
+                Vector3 subGotoPosition = enemyposi.transform.position - muzzlesub.transform.position;
+                Vector3 mainGotoPosition = enemyposi.transform.position - muzzlemain.transform.position;
+
                 // 弾丸の複製
                 GameObject bulletsmain = GameObject.Instantiate(bullet) as GameObject;
                 GameObject bulletssub = GameObject.Instantiate(bullet) as GameObject;
 
                 // Rigidbodyに力を加えて発射
-                bulletsmain.GetComponent<Rigidbody>().AddForce(Vector3.up * speed);
-                bulletssub.GetComponent<Rigidbody>().AddForce(Vector3.down * speed);
+                bulletsmain.GetComponent<Rigidbody>().AddForce(mainGotoPosition * speed);
+                bulletssub.GetComponent<Rigidbody>().AddForce(subGotoPosition * speed);
 
                 // 弾丸の位置を調整
                 bulletsmain.transform.position = muzzlemain.position;
                 bulletssub.transform.position = muzzlesub.position;
 
-                yield return new WaitForSeconds(0.3f);
+                yield return new WaitForSeconds(1f);
             }
         }
 
