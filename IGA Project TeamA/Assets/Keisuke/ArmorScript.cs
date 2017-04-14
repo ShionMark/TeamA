50c060326d7a41cbf25060136aa0d15a8a5f513b
Itsuki.Mekaru <ruizu.vogt@gmail.com>
2017-04-14 10:43:32 +0900

    Asukaの変更を追加

--- IGA Project TeamA/Assets/Keisuke/ArmorScript.cs
+++ IGA Project TeamA/Assets/Keisuke/ArmorScript.cs
@@ -7,7 +7,7 @@ public class ArmorScript : MonoBehaviour {
     void OnCollisionEnter(Collision collision)
     {
         //Explosion only when you hit the specified object
-        if (collision.gameObject.name == "Fallingobject")
+        if (collision.gameObject.name == "Bullet")
         {
             Destroy(this.gameObject);
             GameObject.Destroy(collision.gameObject);
