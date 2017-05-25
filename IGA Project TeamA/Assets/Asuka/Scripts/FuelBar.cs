fe1430e0d62a43d2255f90cd9c886692ac33c1fc
KBC11606120\kbc11606120 <asuka-m-16d@stu.kbc.ac.jp>
2017-05-25 14:53:57 +0900

    UI作成

---
+++ IGA Project TeamA/Assets/Asuka/Scripts/FuelBar.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelBar : MonoBehaviour
+{
+
+    const float GBARANIME = 0.05f;//回復の上昇アニメ
+    const float RBARANIME = 0.03f;//ダメージの赤ゲージアニメ
+    const float DAMAGE1 = 0.15f;//ダメージ
+    const float RECOBAR1 = 0.30f;//回復量
+
+    public Image FuelBarGreen;
+    public Image FuelBarRed;
+    float Gbar, Rbar,Recobar;
+    bool damage,recove;
+
+    // Use this for initialization
+    void Start()
+    {
+        damage = false;
+        recove = false;
+        Gbar = FuelBarGreen.transform.localScale.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Gbar >= 0 && recove == false) Gbar -= 1 * Time.deltaTime / 40; //徐々に減らす
+
+        if (damage == true)
+        {
+            if (Rbar >= Gbar)
+            {
+                Rbar -= RBARANIME;
+            }
+            else
+            {
+                Rbar = Gbar;
+                damage = false;
+            }
+        }
+
+        if (recove == true)
+        {
+            if (Recobar >= Gbar && Gbar < 1)
+            {
+                Gbar = (Gbar <= 0.95) ? Gbar + GBARANIME : 1;
+            }
+            else
+            {
+                recove = false;
+            }
+        }
+
+        if (recove == false && damage == false) Rbar = Gbar;
+
+        FuelBarGreen.transform.localScale = new Vector3(transform.localScale.x, Gbar, transform.localScale.z);
+        FuelBarRed.transform.localScale = new Vector3(transform.localScale.x, Rbar, transform.localScale.z);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && Gbar > 0)//ダメージ(1キー)
+        {
+            Gbar = (Gbar >= DAMAGE1) ? Gbar -= DAMAGE1 : 0.0f;
+            damage = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && Gbar > 0 && Gbar <= 1)//回復(2キー)
+        {
+            Recobar = (Gbar <= 1 - RECOBAR1) ? Gbar + RECOBAR1 : 1.0f;
+            recove = true;
+        }
+    }
+}
---
+++ IGA Project TeamA/Assets/Asuka/Scripts/Score.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    Text ScoreText;
+    Text HScoreText;
+
+    public static int score;
+    private static int highScore;
+
+    private string key = "HIGH SCORE";
+
+    // Use this for initialization
+    void Start()
+    {
+        score = 0;
+        ScoreText = GetComponent<Text>();
+        HScoreText = GetComponent<Text>();
+
+        highScore = PlayerPrefs.GetInt(key, 0);//ハイスコアの読み込み
+        HScoreText.text = "HighScore: " + highScore.ToString();//ハイスコアの表示
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) score++;//マウスクリック時にポイント加算
+        if (Input.GetKeyDown(KeyCode.Alpha0)) highScore = 0;//ハイスコアの初期化(0キー)
+
+
+        ScoreText.text = "HighScore : " + highScore.ToString() + "\n        Score : " + score; //表示
+
+        if (score > highScore)
+        {
+            //ハイスコア更新
+            highScore = score;
+            //ハイスコアを保存
+            PlayerPrefs.SetInt(key, highScore);
+        }
+    }
+}
