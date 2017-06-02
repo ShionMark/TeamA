afd741bb0d3f2c439b8e345b589f9e5bbb7dbc9d
EisukeTaira <eisuke-t-16d@kbc.ac.jp>
2017-06-02 12:33:04 +0900

    コメントアウト

--- IGA Project TeamA/Assets/Touryou/player.cs
+++ IGA Project TeamA/Assets/Touryou/player.cs
@@ -2,68 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
-public class　Touryou : MonoBehaviour {
+//public class player : MonoBehaviour
+////{
+////    public float speed = 2f;
+////    public float width = 6f;
+////    public float heignt = 6f;
+////    private Vector3 position;
 
-    //public float speed = 2f;
-    //public float width = 6f;
-    //public float heignt = 6f;
+////    // スクリーン座標をワールド座標に変換した位置座標
+////    private Vector3 screenToWorldPointPosition;
 
-    //private Vector3 position;
-    //// スクリーン座標をワールド座標に変換した位置座標
-    //private Vector3 screenToWorldPointPosition;
+////    // Update is called once per frame
+////    void OnMouseDrag()
+////    {
+////        float x = Mathf.Cos(Time.time * speed) * width;
+////        float y = Mathf.Sin(Time.time * speed) * heignt;
+////        float z = 0f;
+////        // transform.position = new Vector3(x, y, z);
 
-    //// Update is called once per frame
-    //void OnMouseDrag () {
-
-    //    float x = Mathf.Cos(Time.time * speed) * width;
-    //    float y = Mathf.Sin(Time.time * speed) * heignt;
-    //    float z = 0f;
-    //   // transform.position = new Vector3(x, y, z);
-
-    //    transform.Rotate(new Vector3(0, 0, 3));
-    //    // Vector3でマウス位置座標を取得する
-    //    position = Input.mousePosition;
-    //    // Z軸修正
-    //    position.z = 15f;
-    //    // マウス位置座標をスクリーン座標からワールド座標に変換する
-    //    screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
-    //    // ワールド座標に変換されたマウス座標を代入
-    //    gameObject.transform.position = screenToWorldPointPosition;
-
-
-
-
-    //}
-}
-=======
-public class player : MonoBehaviour
-{
-    public float speed = 2f;
-    public float width = 6f;
-    public float heignt = 6f;
-    private Vector3 position;
-
-    // スクリーン座標をワールド座標に変換した位置座標
-    private Vector3 screenToWorldPointPosition;
-
-    // Update is called once per frame
-    void OnMouseDrag()
-    {
-        float x = Mathf.Cos(Time.time * speed) * width;
-        float y = Mathf.Sin(Time.time * speed) * heignt;
-        float z = 0f;
-        // transform.position = new Vector3(x, y, z);
-
-        transform.Rotate(new Vector3(0, 0, 3));
-        // Vector3でマウス位置座標を取得する
-        position = Input.mousePosition;
-        // Z軸修正
-        position.z = 15f;
-        // マウス位置座標をスクリーン座標からワールド座標に変換する
-        screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
-        // ワールド座標に変換されたマウス座標を代入
-        gameObject.transform.position = screenToWorldPointPosition;
-    }
-}
->>>>>>> master
+////        transform.Rotate(new Vector3(0, 0, 3));
+////        // Vector3でマウス位置座標を取得する
+////        position = Input.mousePosition;
+////        // Z軸修正
+////        position.z = 15f;
+////        // マウス位置座標をスクリーン座標からワールド座標に変換する
+////        screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+////        // ワールド座標に変換されたマウス座標を代入
+////        gameObject.transform.position = screenToWorldPointPosition;
+////    }
+//}
